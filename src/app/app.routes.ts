efccebebd333ffdad28857d2e943bc811e4134a1
luis-keny <luis.lucero03@outlook.com>
2025-05-11 17:24:30 -0500

    feat: Implement initial app structure with routing and styling
    - Added title styling in app.component.css for better presentation.
    - Updated app.component.html to display the title "PokeApi" with a span.
    - Modified app.component.ts to include a title property.
    - Configured app.routes.ts with routes for Pokemon list, detail, and 404 page.
    - Enhanced app.config.ts to provide HTTP client support.
    - Updated index.html to include Google Fonts and Material Icons.
    - Refined global styles in styles.css for consistent typography and layout.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { Page404Component } from './page-404/page-404.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PokemonListComponent,
+    title: 'PokeApi',
+  },
+  {
+    path: ':id',
+    component: PokemonDetailComponent,
+    title: 'PokeApi - detail',
+  },
+  {
+    path: '**',
+    component: Page404Component,
+    title: 'PokeApi - not found',
+  },
+];
