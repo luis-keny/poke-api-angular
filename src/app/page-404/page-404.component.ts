651d70179fa57ac04130b20285472f4b5b445b83
luis-keny <luis.lucero03@outlook.com>
2025-05-11 17:25:52 -0500

    feat: Add 404 page component with styling and tests

---
+++ src/app/page-404/page-404.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-page-404',
+  imports: [RouterLink],
+  templateUrl: './page-404.component.html',
+  styleUrl: './page-404.component.css'
+})
+export class Page404Component {
+
+}
